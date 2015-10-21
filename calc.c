18cf32cd4618a8f9a474f5e06f8db8142fd90061
Michael Bodnar <michael.r.bodnar@gmail.com>
2015-10-21 07:50:04 -0400

    factored the calculator stuff into its own function, added test-cases and CLI program wrapper

--- calc.c
+++ calc.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-
-void main() {
-
-    char inputstring[100];
+int exec_calculator(const char *inputStr)
+{
     char firstDigit[100];
     char secondDigit[100];
 
@@ -18,14 +16,12 @@ void main() {
     int m = 0;
     int operatorFound = 0;
 
-    scanf("%s", &inputstring);
-
-    for (int i = 0; inputstring[i] != '\0'; i++)
+    for (int i = 0; inputStr[i] != '\0'; i++)
     {
-        if ((inputstring[i] == '+' || inputstring[i] == '-' || inputstring[i] == '*' || inputstring[i] == '/') && operatorFound == 0 && i != 0)
+        if ((inputStr[i] == '+' || inputStr[i] == '-' || inputStr[i] == '*' || inputStr[i] == '/') && operatorFound == 0 && i != 0)
         {
             operator_position = i;
-            operation = inputstring[i];
+            operation = inputStr[i];
             operatorFound = 1;
         }
         stringlength = i + 1;
@@ -33,7 +29,7 @@ void main() {
 
     for (int j = 0; j <= operator_position; j++)
     {
-        firstDigit[j] = inputstring[j];
+        firstDigit[j] = inputStr[j];
         if (j == operator_position)
         {
             firstDigit[j] = '\0';
@@ -41,8 +37,8 @@ void main() {
         }
     }
 
-    for (k = operator_position + 1; inputstring[k] != '\0'; k++) {
-        secondDigit[m] = inputstring[k];
+    for (k = operator_position + 1; inputStr[k] != '\0'; k++) {
+        secondDigit[m] = inputStr[k];
         m++;
     }
     secondDigit[k + 1] = '\0';
@@ -54,16 +50,72 @@ void main() {
 
     switch (operation)
     {
-    case '+': printf("%d\n", firstDigitNum + secondDigitNum);
-        break;
-    case '-': printf("%d\n", firstDigitNum - secondDigitNum);
-        break;
-    case '*': printf("%d\n", firstDigitNum * secondDigitNum);
-        break;
-    case '/': printf("%d\n", firstDigitNum / secondDigitNum);
-        break;
+    case '+': return firstDigitNum + secondDigitNum;
+    case '-': return firstDigitNum - secondDigitNum;
+    case '*': return firstDigitNum * secondDigitNum;
+    case '/': return firstDigitNum / secondDigitNum;
+    default: return 0;
     }
 
+    return 0;
+}
 
-    return;
+struct test_case_t
+{
+    const char *expr;
+    int expected_result;
+};
+
+#define NELEMENTS(a) (sizeof(a) / sizeof(a[0]))
+
+int main(int argc, char **argv)
+{
+    int i;
+
+    /*
+     * We can run in two ways:
+     *
+     * 1 - accept a doubled-quote-string argument, like:
+     *      .\our-program.exe "1 + 1"
+     *
+     * 2 - accept no arguments and run the hard-coded test-cases, like:
+     *      .\our-program.exe
+     */
+
+    if (argc == 2)
+    {
+        printf("calculating \"%s\"\n", argv[1]);
+        printf("%d\n", exec_calculator(argv[1]));
+    }
+
+    struct test_case_t testCases[] =
+    {
+        /* ez stuff */
+        { "1 + 0", 1 },
+        { "1 * 0", 0 },
+        { "3 * 3", 9 },
+        { "0 - 100", -100 },
+        { "1-0", 1 },
+        { "0 -1", -1 },
+        { "-1 - 0", -1 },
+        { "-1 * 7", -7 },
+        /* time to break it */
+        { "1 / 2", 1 },     /* integer math w/ rounding or floating-point? */
+        { "1 / 0", 0 },     /* uh oh */
+        { "1", 1 }          /* this is reasonable */
+    };
+
+    for (i = 0; i < NELEMENTS(testCases); ++i)
+    {
+        const struct test_case_t *t = &testCases[i];
+
+        int result = exec_calculator(t->expr);
+
+        printf("TEST CASE \"%s = %d\": %s\n",
+            t->expr,
+            t->expected_result,
+            (t->expected_result == result) ? "PASS" : "FAIL");
+    }
+
+    return 0;
 }
